eb800576245c5871fa64ca9d179fdbd419ccb438
EktaMerai <ektamerai83@gmail.com>
2023-11-23 12:52:55 +0530

    closure

---
+++ concept.js
@@ -0,0 +1,19 @@
+// clouser
+
+
+function outer(){
+    var name = "Hey ekta here"
+    function inner(){
+        console.log(name);
+    }
+    inner();
+}
+
+outer()
+
+//call,apply,blind
+
+
+
+
+
